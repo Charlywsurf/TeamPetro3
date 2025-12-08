3ea2e2256d3a1292287dc1018e09a3b6088c0d1c
charly <csgiba@gmail.com>
2025-12-08 01:38:59 -0300

    new keys

--- main_tienda.js
+++ main_tienda.js
@@ -4,6 +4,8 @@ import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js
 const supabaseUrl = "https://nhvyyvnxrupokuyjunhp.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5odnl5dm54cnVwb2t1eWp1bmhwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyODYyNjEsImV4cCI6MjA3MDg2MjI2MX0.NX2t8xNswjkrggauvmxomQSLoDkiE_Hdaool5Gu0m2o";
+
+
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 async function obtenerProductos() {
